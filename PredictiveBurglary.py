8110fcbe7c61173f51fb4424d8a8d5b38a1f8fb4
bumble156 <chazlonsdale2@hotmail.co.uk>
2017-04-27 15:24:49 +0100

    PredictiveBurglary.py
    The final hand-in, the script was cleaned up slightly and commented using pydoc to make it easy to use for anyone that will use it in the rest of the project.

--- PredictiveBurglary.py
+++ PredictiveBurglary.py
@@ -5,28 +5,70 @@ path = r"C:\PythonWorkspace\PDProject"
 arcpy.env.workspace = path
 
 # Map variables
-bufferSize = "300 Meters" # The size of the buffer around the initial burglary
-threshold = 80 # The threshold value for a burglar to move out of a cluster
-iterations = 10 # The number of clusters to be created
-backgroundBurglaries = 100 # The number of background burglaries to be created
-minBurglaries = 3 # The minimum number of burglaries in a cluster
-maxBurglaries = 12 # The maximum number of burglaries in a cluster
+bufferSize = "300 Meters"
+"""
+str: The size of the buffer around the initial burglary.
+"""
+threshold = 80
+"""
+int: The threshold value out of 100, above which the burglar will move from a cluster.
+"""
+iterations = 10
+"""
+int: The number of clusters to be created.
+"""
+backgroundBurglaries = 100
+"""
+int: The number of background burglaries to be created.
+"""
+minBurglaries = 3
+"""
+int: The minimum number of burglaries in a cluster.
+"""
+maxBurglaries = 12
+"""
+int: The maximum number of burglaries in a cluster.
+"""
+prevPoint = (0,0)
+"""
+point: Previous location of initial burglary.
+"""
 
 # Monte Carlo variables
 maxNum = 0
+"""
+int: The maximum value in the sample file.
+"""
 minNum = 0
-interval = 0
+"""
+int: The minimum value in the sample file.
+"""
+interval = 0.0
+"""
+float: The interval for the sample file ((max - min) / numberOfBins).
+"""
 numberOfBins = 20
+"""
+int: The number of bins to separate the sample file into.
+"""
 bins = [[0.0 for x in range(numberOfBins)] for y in range(2)]
+"""
+list: Create the bins list, initialised as 0.0.
+"""
 
-# Previous location of initial burglary
-prevPoint = (0,0)
-
-
-# The following classes for performing Monte Carlo sampling are adapted from the work of Sim Reaney and Andy Evans at
-# http://www.geog.leeds.ac.uk/courses/other/programming/info/code/modelling/calibration/MonteCarlo.java
-# Set the frequency distribution of numbers returned by the sampler
 def setNumbers(filename):
+    """
+
+    Function to set the frequency distribution of numbers returned by the sampler.
+    For use with Monte Carlo Sampling.
+
+    Adapted from the work of Sim Reaney and Andy Evans at
+    http://www.geog.leeds.ac.uk/courses/other/programming/info/code/modelling/calibration/MonteCarlo.java
+
+    Args:
+        filename (str): The location of the file containing the sample data.
+        
+    """
     data = fileAsArray(filename)
     
     i = 0
@@ -43,8 +85,22 @@ def setNumbers(filename):
         bins[1][i] = bins[1][i] / len(data)
         i = i + 1
 
-# Returns a number at a rate determined by a preset distribution
 def getNumber(random):
+    """
+
+    Function to return a number at a rate determined by a preset distribution.
+    For use with Monte Carlo Sampling.
+
+    Adapted from the work of Sim Reaney and Andy Evans at
+    http://www.geog.leeds.ac.uk/courses/other/programming/info/code/modelling/calibration/MonteCarlo.java
+
+    Args:
+        random (int): A random number between 0 and 1 to be used in selecting a bin.
+
+    Returns:
+        value (float): The result of the Monte Carlo sampling using the random input.
+        
+    """
     value = 0.0
     i = 0
     
@@ -57,8 +113,22 @@ def getNumber(random):
 
     return value
 
-# Returns a file as a one-dimensional array
 def fileAsArray(filename):
+    """
+
+    Function to return a file as a one-dimensional array.
+    For use with Monte Carlo Sampling.
+
+    Adapted from the work of Sim Reaney and Andy Evans at
+    http://www.geog.leeds.ac.uk/courses/other/programming/info/code/modelling/calibration/MonteCarlo.java
+
+    Args:
+        filename (str): The location of the file containing the sample data.
+
+    Returns:
+        data (int[]): The data in the file as a one-dimensional array. 
+        
+    """
     data2d = genfromtxt(filename, delimiter=',')
     
     position = 0
@@ -87,10 +157,14 @@ def fileAsArray(filename):
     return data
 
 
+def setup():
+    """
 
-# Function to be called when creating a new model
-# Creates required initial shape files and empties them if they already exist
-def setup():    
+    Function to be called when creating a new model.
+    Creates required initial shape files and empties them if they already exist.
+    Sets up the Monte Carlo sampling distribution for later use.
+        
+    """
     # Creates the feature class for the boundary
     if arcpy.Exists("boundary.shp"):
         print "Boundary file exists"
@@ -113,9 +187,11 @@ def setup():
 
     # Creates the feature class for the burglaries
     if arcpy.Exists("burglaries.shp"):
+        # Clears the burglary file
         arcpy.DeleteFeatures_management("burglaries.shp")
         print "Burglaries file exists"
     else:
+        # Creates the burglary file
         arcpy.CreateFeatureclass_management(path, "burglaries.shp", "POINT")
         arcpy.AddField_management("burglaries.shp", "ITER", "SHORT")
         arcpy.AddField_management("burglaries.shp", "TYPE", "TEXT")
@@ -123,9 +199,11 @@ def setup():
 
     # Creates the feature class for the buffers
     if arcpy.Exists("buffers.shp"):
+        # Clears the buffers file
         arcpy.DeleteFeatures_management("buffers.shp")
         print "Buffers file exists"
     else:
+        # Creates the buffers file
         arcpy.CreateFeatureclass_management(path, "buffers.shp", "POLYGON")
         arcpy.AddField_management("buffers.shp", "ITER", "SHORT")
         print "Buffers file created"
@@ -133,10 +211,21 @@ def setup():
     # Creates the frequency distribution for the Monte Carlo sampling
     setNumbers(path + r'\testData.txt')
         
-
-# Function to add random points within a boundary and append to the burglaries shape file
-# Note the interim file must be separately deleted before it can be reused
 def randomPoints(interimName, boundary, numPoints, iterVal, pointType):
+    """
+
+    Function to add random points within a boundary and append to the burglaries shape file.
+    Note the interim file must be separately deleted before it can be reused.
+    This is because it may be required again in a later step.
+
+    Args:
+        interimName (str): The name of the interim shapefile to be created to hold the random points.
+        boundary (str): The name of the boundary shapefile to create the points within.
+        numPoints (int): The number of points to be created.
+        iterVal (int): The number of the current iteration to be added to the attribute table for the points created.
+        pointType (str): The type of point being created (INITIAL, SECONDARY, BACKGROUND) to be added to the attribute table for the points created.
+        
+    """
 
     global prevPoint
     
@@ -157,13 +246,26 @@ def randomPoints(interimName, boundary, numPoints, iterVal, pointType):
     # Adds the subsequent points into the burglaries.shp class that will contain all burglaries        
     arcpy.Append_management(interimName, "burglaries.shp", "NO_TEST")
 
-# Function to add a specific point at a distance from the previous initil burglary point to the burglaries shape file
-# Note the interim file must be separately deleted before it can be reused
+
 def specificPoint(distance, bearing, interimName, boundary, iterVal):
+    """
+
+    Function to add a specific point at a distance from the previous initil burglary point to the burglaries shape file.
+    Note the interim file must be separately deleted before it can be reused.
+    This is because it may be required again in a later step.
+
+    Args:
+        distance (float): The distance from the previous point to create the new point in meters.
+        bearing (int): The bearing from the last point to create the new point (0-360)
+        interimName (str): The name of the interim shapefile to be created to hold the random points.
+        boundary (str): The name of the boundary shapefile to create the points within.
+        iterVal (int): The number of the current iteration to be added to the attribute table for the points created.
+        
+    """
 
     global prevPoint
 
-    # Generate a random bearing from the previous point
+    # Calculate the angle and bearing from the previous point
     angle = 90 - bearing
     bearing = math.radians(bearing)
     angle =   math.radians(angle)
@@ -204,10 +306,17 @@ def specificPoint(distance, bearing, interimName, boundary, iterVal):
         else:
             # Try again using the same function parameters but a new random bearing value
             specificPoint(distance, random.randint(0, 360), interimName, boundary, iterVal)
-
-
-# Funtion to add a new burglary cluster within the boundary    
+   
 def newZone(distance):
+    """
+
+    Funtion to add a new burglary cluster within the boundary.
+    Distance can be set to 0 if the point being added is to be the first in the area.
+
+    Args:
+        distance (float): The distance from the previous point to create the new point in meters.
+        
+    """
     if distance == 0:
         # Creates the first random burglary point
         randomPoints("initPoint.shp", "boundary.shp", 1, iteration, "INITIAL")
@@ -215,7 +324,7 @@ def newZone(distance):
         # Creates subsequent burglary point at specific position
         specificPoint(distance, random.randint(0, 360), "initPoint.shp", "boundary.shp", iteration)
     
-    # Creates the buffer around the initial point
+    # Creates the buffer around the initial point in a  cluster
     arcpy.Buffer_analysis("initPoint.shp", "initBuff.shp", bufferSize)
 
     # Adds the buffer into the buffers.shp class that will contain all buffers
@@ -242,8 +351,12 @@ def newZone(distance):
     # Delete the initial buffer so the class can be reused
     arcpy.Delete_management("initBuff.shp")
 
-# Function to generate random burglaries inside the boundary not related to a cluster
 def randomNoise():
+    """
+
+    Function to generate random burglaries inside the boundary not related to a cluster.
+    
+    """
     # Creates the background burglaries (i.e. random noise)
     randomPoints("randomNoise.shp", "boundary.shp", backgroundBurglaries, 0, "BACKGROUND")
 
